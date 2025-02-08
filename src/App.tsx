b7c091c2a849f760f4ebe5cbac44329f997bc900
Prajwal-PL <prajwalpl096@gmail.com>
2025-02-08 11:35:09 +0530

    Initializing project, Installing ShadCN UI, Adding Tailwind CSS@3, Testing components

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import { Button } from "./components/ui/button";
+
+function App() {
+  return (
+    <div>
+      <h1 className="text-green-500 font-bold">Hello World</h1>
+      <Button>Click Me</Button>
+    </div>
+  );
+}
+
+export default App;
